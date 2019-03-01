0b8e4e7101c6160bd38ae7c89926c9709803ec67
Richie Bendall <richiebendall@gmail.com>
2019-03-01 19:06:32 +1300

    Add braille preset

---
+++ presets/braille.js
@@ -0,0 +1,6 @@
+// Braille Preset | Copyright (c) 2019 Richie Bendall
+module.exports = {
+    format: ' {bar} {percentage}% | ETA: {eta}s | {value}/{total}',
+    barCompleteChar: '\u28FF',
+    barIncompleteChar: '\u28C0'
+};
