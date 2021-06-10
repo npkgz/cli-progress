81a8aeef569dcb02ed127191388e406a30e19406
MatievisTheKat <matievisthekat@gmail.com>
2021-06-10 21:32:16 +0200

    change indent size

--- presets/shades-classic.js
+++ presets/shades-classic.js
@@ -1,6 +1,6 @@
 // cli-progress legacy style as of 1.x
 module.exports = {
-    format: ' {bar} {percentage}% | ETA: {eta} | {value}/{total}',
-    barCompleteChar: '\u2588',
-    barIncompleteChar: '\u2591'
+   format: ' {bar} {percentage}% | ETA: {eta} | {value}/{total}',
+   barCompleteChar: '\u2588',
+   barIncompleteChar: '\u2591'
 };
